91da6826d4de5c301ebe27b573e7729c3485cd31
Han Chuanhua <hanch@localhost.localdomain>
2018-04-25 16:37:40 +0800

    modify main.c and README

--- hello_pro/src/main/main.c
+++ hello_pro/src/main/main.c
@@ -5,8 +5,11 @@
 int main(int argc,char **argv)
 {
 
+	
+    puts("start hello!!!");
     printf("add(4,3)=%d,sub(4,3)=%d\n",\
 				add(4,3),sub(4,3));
+    puts("end hello!!!");
      return 0;
 }
 

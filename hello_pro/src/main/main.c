af02b36c1b8dc1a80a4c1b91179ba225066a3961
Chanhua Han <chanhua.han@nxp.com>
2018-06-13 19:04:50 +0800

    modify main.c
    Signed-off-by: Chanhua Han <chanhua.han@nxp.com>

--- hello_pro/src/main/main.c
+++ hello_pro/src/main/main.c
@@ -7,7 +7,7 @@ int main(int argc,char **argv)
 
 	
     puts("start hello!!!");
-    printf("add(4,3)=%d,sub(4,3)=%d\n",\
+    printf("add(4,3)=%3d,sub(4,3)=%6d\n",\
 				add(4,3),sub(4,3));
     puts("end hello!!!");
      return 0;
